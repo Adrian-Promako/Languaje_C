1537197653eac198949252097025ae2c9638c8ce
Adrian-Promako <gadrianmoreno@ucompensar.edu.co>
2026-09-09 22:33:01 -0500

    Add another exercices

---
+++ EjemplosC/FILE_GestorC/escritura.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+// creamos el puntero para escribir el archivo y  lo arios n modo escritura
+    FILE *escritura = fopen ("inventario.txt", "w");
+
+    // verificamos que exista el archivo para poder escribir en el.
+    if (escritura != NULL)  {
+
+        //Usa "fputs" paa escribir un titulo plano en el archivo inventario.txt
+        fputs("ID_PODUCTO CANTIDAD PRECIOS \n", escritura);
+
+        // Usa fprintf para escribir un registro numerico con formato
+        fprintf(escritura, "%d %d %.2df\n", 1001, 15, 24.50);
+
+        fclose(escritura);
+    }
+// declaramos variables locales para guardar los datos que recuperaremos desde el archivo
+
+char encabezado[100];
+int id, cantidad;
+float precio;
+
+//Ahora volvemos a abrir el mismo documento pero en modo lectura "r" para verificar su contenido
+
+    FILE *lectura = fopen("inventario.txt", "r");
+    // verifica que el archivo de lectura haya sido encontrado y abierto
+    if (lectura != NULL) {
+        // usa "fgets" para extraer la primera linea completa
+        fgets(encabezado, sizeof(encabezado), lectura);
+        printf ("encabezado leido con fgets: %s", encabezado);
+
+        //usaremos "fscanf" para extraer las segunda linea analizando los tipos de datos exactos
+        fscanf(lectura, "%d %d %f", &id, &cantidad, &precio);
+        //imprime los datos ya con el formato deseado
+        printf ("Datos leidos con fscanf -> ID: %d | Cantidad: %d | precio: $%.2f\n", id, cantidad, precio);
+            
+        fclose(lectura);
+    }
+    return 0;        
+}
--- EjemplosC/FILE_GestorC/pruebaC.c
+++ EjemplosC/FILE_GestorC/pruebaC.c
@@ -6,12 +6,12 @@ int main() {
 
     char uno [10], dos [10], tres[10];
 
-/*Para el suo de la funcion de lectura fscanf se requiere tres cosas:
+/*Para el uso de la funcion de lectura fscanf se requiere tres cosas:
 el puntero, el formato de los datos y el nombre de las variables involucradas.*/
     if (fp != NULL) {
         fscanf(fp, "%s %s %s", uno, dos, tres);
         printf("Los valores son:\n %s\n %s\n %s\n", uno, dos, tres);
-        return 0;
+        return 0; //indica que se ejecuto correctamente
      } else {
             printf("archivo no creado\n");    
             return 1;
